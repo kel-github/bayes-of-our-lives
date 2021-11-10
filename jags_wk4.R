9f4a807beb4a7ba9f0c422a91265a1817f4f536e
Kelly <getkellygarner@gmail.com>
2021-11-10 19:29:08 +1000

    JAGS things

---
+++ jags_wk4.R
@@ -0,0 +1,36 @@
+rm(list=ls())
+
+# 1. Specify the model
+library(rjags)
+
+mod_string = " model {
+  for (i in 1:n) {
+    y[i] ~ dnorm(mu, 1.0/sig2) 
+  }
+  mu ~ dt(0.0, 1.0/1.0, 1)
+  sig2 = 1.0
+} "
+
+# 2. set up the model
+set.seed(42)
+y <- rnorm(10, 1, 2)
+n = length(y)
+
+data_jags = list(y=y, n=n)
+params = c("mu")
+
+inits <- function(){
+  inits <- list("mu"= 0.0)
+}
+
+mod = jags.model(textConnection(mod_string), data = data_jags, init=inits)
+
+# 3. Run the MCMC sampler
+update(mod, 500)
+mod_sim = coda.samples(model = mod, variable.names = params, n.iter = 1000)
+
+# 4. Post processing
+library(coda)
+
+plot(mod_sim)
+summary(mod_sim)
